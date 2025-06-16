ffeae3f2576d713057a0e6d538e2ca02620a5f57
Kaleb <Kalebreid15@gmail.com>
2025-06-16 09:43:35 -0400

    created class car & set values and printed to console

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            
+            //Set the object's properties
+            myCar.Make = "Toyota";
+            myCar.Model = "Camry";
+            myCar.Year = 2020;
+            
+            //Print the values on the Console
+            Console.WriteLine($"Make: {myCar.Make}");
+            Console.WriteLine($"Model: {myCar.Model}");
+            Console.WriteLine($"Year: {myCar.Year}");
+            
         }
+    }
+
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        
     }
 }
